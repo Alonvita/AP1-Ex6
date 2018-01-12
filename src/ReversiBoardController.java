0332b831bc2a3bb8e478ea29c74aafaca328c761
alon <alon.vita@gmail.com>
2018-01-12 22:00:23 +0200

    graphics v3.0

---
+++ src/ReversiBoardController.java
@@ -0,0 +1,35 @@
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.HBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * ReversiGameController Class.
+ */
+public class ReversiGameController implements Initializable {
+    @FXML
+    private HBox root;
+    private Cell[][] matrix = {
+            {Cell.EMPTY, Cell.EMPTY, Cell.EMPTY, Cell.EMPTY},
+            {Cell.EMPTY, Cell.EMPTY, Cell.EMPTY, Cell.EMPTY},
+            {Cell.EMPTY, Cell.EMPTY, Cell.EMPTY, Cell.EMPTY},
+            {Cell.EMPTY, Cell.EMPTY, Cell.EMPTY, Cell.EMPTY}};
+
+    /**
+     * initialize(URL location, ResourceBundle resources).
+     *
+     * @param location URL -- url;
+     * @param resources ResourceBundle -- resources.
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // Local Variables
+        Board board = new Board(this.matrix);
+        board.setPrefHeight(400);
+        board.setPrefWidth(400);
+        root.getChildren().add(0, board);
+        board.draw();
+    }
+}
